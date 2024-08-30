e09f7a2f5edf09c16f272c0d16adf1511200d6d3
Ndefo-Angelica <ndefoangelica076@gmail.com>
2024-08-30 20:49:39 +0100

    modified

--- src/pages/Home/Footer.tsx
+++ src/pages/Home/Footer.tsx
@@ -1,9 +1,9 @@
 
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <footer className="bg-pink-200 py-10">
@@ -22,32 +22,33 @@ const Footer: React.FC = () => {
           <div className="flex flex-col items-center">
             <h3 className="font-bold text-lg mb-2">Our Partners</h3>
             <ul className="space-y-1 text-sm flex flex-col items-left text-left">
-              <li><a href="#" className="hover:text-pink-300">ISTAMA</a></li>
-              <li><a href="#" className="hover:text-pink-300">ESG</a></li>
-              <li><a href="#" className="hover:text-pink-300">IUC</a></li>
-              <li><a href="#" className="hover:text-pink-300">IUT</a></li>
+            <li><Link to="https://istamacameroon.com/" className="hover:text-pink-500">ISTAMA</Link></li>
+              <li><Link to="https://www.univ-iug.com/" className="hover:text-pink-500">IUG</Link></li>
+              <li><Link to="https://myiuc.com/" className="hover:text-pink-500">IUC</Link></li>
+              <li><Link to="https://www.iut-dla.cm/" className="hover:text-pink-500">IUT</Link></li>
             </ul>
           </div>
 
           <div className="flex flex-col items-center">
             <h3 className="font-bold text-lg mb-2">Informations</h3>
             <ul className="space-y-1 text-sm flex flex-col items-left text-left">
-              <li className="hover:text-pink-300 cursor-pointer" onClick={()=> navigate("/Register")}>Register</li>
-              <li><a href="#" className="hover:text-pink-300">Login</a></li>
-              <li><a href="#" className="hover:text-pink-300">My plans</a></li>
-              <li><a href="#" className="hover:text-pink-300">My courses</a></li>
+            <li><Link to="/register" className="hover:text-pink-500">Register</Link></li>
+              <li><Link to="/login" className="hover:text-pink-500">Login</Link></li>
+              <li><Link to="/
+              " className="hover:text-pink-500">My plans</Link></li>
+              <li><Link to="/" className="hover:text-pink-500">My Courses</Link></li>
             </ul>
           </div>
         </div>
 
-        <div className="flex flex-grow justify-evenly space-x-6 mt-8 ">
-          <a href="https://www.facebook.com/" className="text-white hover:text-pink-300 text-2xl">
-            <img src="src/assets/facebook.png" alt="Facebook" className="h-10" />
+        <div className="flex flex-row space-x-6 pt-5 justify-end content-center px-4">
+            <a href="https://www.facebook.com/" className="text-white hover:text-pink-300 text-2xl content-center">
+              <img src="src/assets/facebook.png" alt="Facebook" className="h-8" />
           </a>
-          <a href="https://x.com/" className="text-white hover:text-pink-300 text-2xl">
-            <img src="src/assets/twitter.png" alt="Twitter" className="h-10" />
+            <a href="https://x.com/" className="text-white hover:text-pink-300 text-2xl content-center">
+              <img src="src/assets/twitter.png" alt="Twitter" className="h-9" />
           </a>
-          <a href="https://www.linkedin.com/" className="text-white hover:text-pink-300 text-2xl">
+            <a href="https://www.linkedin.com/" className="text-white hover:text-pink-300 text-2xl content-center">
             <img src="src/assets/linkedin.png" alt="LinkedIn" className="h-12" />
           </a>
         </div>
